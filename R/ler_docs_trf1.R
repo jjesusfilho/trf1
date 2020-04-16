973757d2ce80b3affa3c7c9c83648df10e18b488
José de Jesus Filho <jjesusfilho@gmail.com>
2020-04-16 10:55:04 -0300

    aperfeiçoa função ler_docs_trf1

--- R/ler_docs_trf1.R
+++ R/ler_docs_trf1.R
@@ -11,26 +11,27 @@
 #' \dontrun{
 #' df <- ler_docs_trf1()
 #' }
-ler_docs_trf1 <- function(arquivos = NULL, diretorio =  "."){
-
-  if (is.null(arquivos)){
-
-    arquivos <- list.files(diretorio,full.names=TRUE)
+ler_docs_trf1 <- function (arquivos = NULL, diretorio = ".")
+{
+  if (is.null(arquivos)) {
+    arquivos <- list.files(diretorio, full.names = TRUE)
   }
 
-  purrr::map_dfr(arquivos,purrr::possibly(purrrogress::with_progress(~{
-
-    processo <- stringr::str_extract(.x,"\\d{20}")
-    documento <- stringr::str_extract(.x,"\\p{L}+(?=_\\d{20})")
-
+  purrr::map_dfr(arquivos, purrr::possibly(purrrogress::with_progress(~{
+    processo <- stringr::str_extract(.x, "\\d{20}")
+    documento <- stringr::str_extract(.x, "(?<=_)\\p{L}.+?(?=_\\d{20})")
     julgado <- textreadr::read_document(.x)
 
-    tibble::tibble(processo,documento, julgado)
+    ## Muitos documentos com extensão doc são, na verdade, docx.
+    if (is.null(julgado) && stringr::str_detect(julgado,"\\.doc$")){
+
+      julgado <- textreadr::read_docx(.x)
+
+    }
+
+    if (is.null(julgado)) julgado <- NA_character_
 
 
-  }),NULL))
-
-
-
-
+    tibble::tibble(processo, documento, julgado)
+  }), NULL))
 }

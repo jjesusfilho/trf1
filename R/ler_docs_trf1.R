8f8066816a0f9fe2f73495472024979776517b3d
José de Jesus Filho <jjesusfilho@gmail.com>
2020-04-17 09:25:01 -0300

    ajuste ler_docs

--- R/ler_docs_trf1.R
+++ R/ler_docs_trf1.R
@@ -16,22 +16,36 @@ ler_docs_trf1 <- function (arquivos = NULL, diretorio = ".")
   if (is.null(arquivos)) {
     arquivos <- list.files(diretorio, full.names = TRUE)
   }
-
   purrr::map_dfr(arquivos, purrr::possibly(purrrogress::with_progress(~{
     processo <- stringr::str_extract(.x, "\\d{20}")
-    documento <- stringr::str_extract(.x, "(?<=_)\\p{L}.+?(?=_\\d{20})")
-    julgado <- textreadr::read_document(.x)
+    data <- stringr::str_extract(.x,"\\d{2}_\\d{2}_\\d{4}") %>%
+      lubridate::dmy()
+    documento <- stringr::str_extract(.x, "(?<=_)\\p{L}.+?(?=_\\d{20})") %>%
+      stringi::stri_trans_general("latin-ascii")
+
+    julgado <- textreadr::read_document(.x,combine = TRUE)
 
     ## Muitos documentos com extensão doc são, na verdade, docx.
-    if (is.null(julgado) && stringr::str_detect(julgado,"\\.doc$")){
+    if (is.null(julgado) && stringr::str_detect(.x,"\\.doc$")){
 
-      julgado <- textreadr::read_docx(.x)
+      julgado <- textreadr::read_docx(.x,combine=TRUE)
 
     }
 
     if (is.null(julgado)) julgado <- NA_character_
 
 
-    tibble::tibble(processo, documento, julgado)
+    df <- tibble::tibble(processo,data, documento, julgado) %>%
+      dplyr::group_by(processo) %>%
+      tidyr::chop(cols = julgado)
+
+
+
+    s <- purrr::map(df$julgado, ~stringr::str_c(.x, collapse = "\n")) %>%
+      unlist()
+
+    df %>% dplyr::mutate(julgado = !!s)
+
+
   }), NULL))
 }
